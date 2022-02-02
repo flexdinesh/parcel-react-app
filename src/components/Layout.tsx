526a1af64c5325f97867baddde2c5144cfef14ea
Dinesh Pandiyan <flexdinesh@gmail.com>
2022-02-02 18:45:29 +1100

    Use react logo as default favicon and logo

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -8,6 +8,8 @@ const Layout: React.FC = ({ children }) => {
       <Global
         styles={`
           body {
+            margin: 0px;
+            padding: 0px;
             font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
               Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
             -webkit-font-smoothing: antialiased;
